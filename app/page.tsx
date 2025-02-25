94dc62a5462193c939f18181b128491dd0ebc003
majazocom <maja.gedda@zocom.se>
2025-02-25 12:04:35 +0100

    usecontext + usereducer

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,7 @@
-'use client';
-
-import { useReducer } from "react";
-
-// 1. definierar reducer-funktionen
-function counterReducer(state: number, action: string) {
-  switch (action) {
-    case "INCREMENT": return state + 1;
-    case "DECREMENT": return state - 1;
-    case "RESET": return 0;
-    default: return state;
-  }
-}
-
-// skapa en komponent som använder sig av counterReducer
-function Counter() {
-  const [count, dispatch] = useReducer(counterReducer, 0);
-  return (
-    <section>
-      <p>Räknare: {count}</p>
-      <button onClick={() => dispatch("INCREMENT")}>+1</button>
-      <button onClick={() => dispatch("DECREMENT")}>-1</button>
-      <button onClick={() => dispatch("RESET")}>C</button>
-    </section>
-  )
-}
+import React from 'react'
 
 export default function Home() {
-
   return (
-    <main>
-      <Counter />
-    </main>
-  );
+    <div>Home</div>
+  )
 }
